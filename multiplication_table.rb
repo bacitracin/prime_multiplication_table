4ef673fe453b7ae32e1af28512bd7aa2f1bdbea9
bacitracin <tracyctran@gmail.com>
2016-10-15 00:32:05 -0700

    added initial prime and prime array generator functions

---
+++ multiplication_table.rb
@@ -0,0 +1,30 @@
+def print_table()
+end
+
+def pull_primes(num)
+  primes_array = []
+  counter = 2
+
+  until primes_array.size == num
+    if is_prime?(counter)
+      primes_array << counter
+    end
+    counter += 1
+  end
+
+  primes_array
+
+end
+
+def is_prime?(num)
+  divisors_array = []
+  
+  (1..num).each do |n|
+    if num % n == 0
+      divisors_array << num
+    end
+  end
+
+  divisors_array.length > 2 ? false:true
+
+end
