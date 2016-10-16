5bfae303381319ea465d3a3ca6f7c276be9fb4b0
bacitracin <tracyctran@gmail.com>
2016-10-16 00:56:36 -0700

    fix require stringio in spec_helper, set up print_table tests

--- multiplication_spec.rb
+++ multiplication_spec.rb
@@ -1,3 +1,4 @@
+require_relative 'spec_helper'
 require 'rspec'
 require_relative 'lib/multiplication_table'
 
@@ -42,15 +43,21 @@ describe MultiplicationTable do
 
   describe "#print_table" do
 
-    context 'testing with num = 1' do
-      it 'returns a 1 x 1 table' do
-        expect(@multiplication_table.print_table(1).include?(4))
+    context 'testing with num = 4' do
+      it 'prints a 4 x 4 table' do
+        printed = capture_stdout do
+          @multiplication_table.print_table(4)
+        end
+        printed.should eq("      2    3    5    7   \n\n2  |  4    6    10   14  \n\n3  |  6    9    15   21  \n\n5  |  10   15   25   35  \n\n7  |  14   21   35   49  \n\n")
       end
     end
 
-    context 'testing with num = 1' do
-      it 'returns a 1 x 1 table' do
-        expect(@multiplication_table.print_table(1).include?(4))
+    context 'testing with num = 2' do
+      it 'prints a 2 x 2 table' do
+        printed = capture_stdout do
+          @multiplication_table.print_table(2)
+        end
+        printed.should eq("      2    3   \n\n2  |  4    6   \n\n3  |  6    9   \n\n")
       end
     end
 
